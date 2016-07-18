ab278bf81b9d3a98bbefaf0733a74f865a18e5ab
Stephen Pascoe <Stephen.Pascoe@nanoporetech.com>
2016-07-18 15:32:07 +0100

    Checkpoint after a long break

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,22 @@
 module Main where
 
+import Data.Aeson as A
+import qualified Data.ByteString.Lazy as BL
 import Lib
 
-main :: IO ()
+-- Return an error or string to print.
+parseLine :: BL.ByteString -> A.Result Expr
+parseLine str = case A.decode str of
+  Nothing -> Error "ERROR"
+  Just obj -> A.fromJSON obj
+
 main = undefined
+{-
+main :: IO ()
+main = do
+  putStr "> "
+  line <- BL.readLn
+  case parseLine line of
+    A.Error message -> putStrLn $ "ERROR: " ++ message
+    A.Success expr -> putStrLn $ show expr
+-}
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,19 +1,25 @@
-module Main where
+{-# LANGUAGE QuasiQuotes #-}
+
+module MongoSQL.Test where
 
 import Test.Hspec
 import Test.QuickCheck
+import Test.QuickCheck.Instances
 import Lib
 
 import Data.Aeson as A
 import qualified Data.Text as T
-
-instance Arbitrary T.Text where
-  arbitrary = T.pack <$> arbitrary
-  shrink xs = T.pack <$> shrink (T.unpack xs)
+import qualified Data.ByteString.Lazy as BL
+import Text.RawString.QQ
 
 
 fromJSON_eq :: Value -> Expr -> Bool
 fromJSON_eq obj expr = fromJSON obj == A.Success expr
+
+json_eq :: BL.ByteString -> Expr -> Bool
+json_eq str expr = case decode str of
+  Nothing -> False
+  Just obj -> obj == expr
 
 -- toJSON_eq :: Expr -> Value -> Bool
 -- toJSON_eq expr obj = toJSON expr == obj
@@ -26,12 +32,13 @@ prop_eq1 a b = fromJSON_eq (object [ a .= b ]) (ExprEQ a (String b))
 prop_eq2 :: T.Text -> T.Text -> Bool
 prop_eq2 a b = fromJSON_eq (object [ a .= object [ "$eq" .= b ] ]) (ExprEQ a (String b))
 
+
 prop_and1 :: T.Text -> T.Text -> T.Text -> T.Text -> Bool
 prop_and1 a b c d = fromJSON_eq (object [ "$and" .= [object [a .= b], object [c .= d]] ])
                                 (ExprAND [ExprEQ a (String b), ExprEQ c (String d)])
 
 main :: IO ()
-main = hspec $ do
+Main = hspec $ do
   describe "MongoDB JSON query language" $ do
     it "encodes default equality" $ property prop_eq1
     it "encodes equality with $eq" $ property prop_eq2
